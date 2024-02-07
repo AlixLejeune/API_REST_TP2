20a9e240f87c039a2cc09fb8d4d34384f260aef6
AlixLejeune <alix.lejeune@etu.univ-smb.fr>
2024-02-07 09:45:28 +0100

    Ajout des tests de base

---
+++ API_REST_TP2Tests1/Controllers/SeriesControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API_REST_TP2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_REST_TP2.Controllers.Tests
+{
+    [TestClass()]
+    public class SeriesControllerTests
+    {
+        [TestMethod()]
+        public void SeriesControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSeriesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSerieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutSerieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostSerieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteSerieTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
